9d62a917473a342cd4f1ce6cfeb0f59565016a90
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-07-31 03:16:00 +0000

    [Finish] 2-5

--- db/migrate/20230731015830_create_lists.rb
+++ db/migrate/20230731015830_create_lists.rb
@@ -2,6 +2,9 @@ class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
 
+      t.string :title  # title（タイトル）
+      t.string :body  # body（本文）
+      t.string :author  # author (投稿者)
       t.timestamps
     end
   end
